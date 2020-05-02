298967e82cbb7e4cdebd2b883741eab577ff5053
drwinns <winstonchikazhe@gmail.com>
2020-05-02 18:29:08 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+ 
+const wordpress = require('./lib/wordpress/wordpress');
+ 
+const port = process.env.PORT || 3100;
+ 
+let app = express();
+app.use(bodyParser.json());
+ 
+app.post('/webhook', (req, res, next) => {
+	
+	let action = req.body.result.action;
+	let tag = req.body.result.parameters.tags;
+	
+	wordpress.getPosts(tag, (errorMessage, postContent) => {
+		if (errorMessage) {
+	  		res.status(400).send({
+				speech: errorMessage,
+				displayText: errorMessage,
+				source: 'wp-webhook',
+			});
+		} else {
+		  	res.status(200).send({
+				speech: '',
+				displayText: '',
+				source: 'wp-webhook',
+				messages: postContent
+			});
+		}
+	});
+	
+});
+ 
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+});
